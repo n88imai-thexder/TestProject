98b2385367af170cabf4e7b6ec686f3b5d0d2268
n88imai-thexder <n88imai_thexder@ric.hi-ho.ne.jp>
2016-04-09 18:15:52 +0900

    Unityプロジェクトを追加してみたり

---
+++ UnityProject/TestUnity/Assets/Script/TestScript.cs
@@ -0,0 +1,22 @@
+﻿// GITのテスト用
+
+using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		print("TEST TEST");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	
+	}
+}
